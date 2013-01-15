479f49fde6b90142eea9290afda47d7054fd3177
Ozgun Babur <ozgunbabur@gmail.com>
2013-01-15 15:53:55 -0500

    Initial commit.

---
+++ src/main/java/org/cbio/causality/model/PathUser.java
@@ -0,0 +1,12 @@
+package org.cbio.causality.model;
+
+/**
+ * While a path is being built, if a PathUSer was provided, its processPath method is called at each
+ * addition to the path.
+ *
+ * @author Ozgun Babur
+ */
+public interface PathUser
+{
+	public void processPath(Path path);
+}
